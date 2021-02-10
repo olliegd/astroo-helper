311f68033047a18d8eea16c6573dcc1fcf4cee08
Ollie Downes <77174048+olliegd@users.noreply.github.com>
2021-02-10 14:39:51 +0000

    Commit

--- commands/supportconfig.js
+++ commands/supportconfig.js
@@ -1,25 +1,19 @@
-const { executionAsyncResource } = require("async_hooks")
+module.exports = {
+    name: 'supportconfig',
+    description: "Displays the support message",
+    execute(message, args, Discord) {
+        if (message.author.bot) return;
 
-module.exports.run = async (client, message, args) => {
-    if (message.author.bot) return;
+        let embed = new Discord.MessageEmbed()
+            .setAuthor('Astroo Support.')
+            .setColor('039dfc')
+            .addFields(
+                { name: '1.', value: ' ***I cant invite the bot to my server.***', },
+                {name: '2', value: '***Astroo isn\'t responding***', },
+                {name: '3', value: '***I cant hear Astroo or the sound is glitching. ***'}
+            )
 
-    let embed = new Discord.MessageEmbed()
-        .setAuthor('Support.')
-        .setColor('000000')
-        .addField('Commands', 'Run to get a list of commands.', true)
-        .addField("Moderation", 'Only Staff can run this command.', true)
-        .addField("Fun", `Get a list of fun commands.`, true)
-        .addField('Who Are We?', 'Project demi is a range of softwares founded in December 2020. Check #about for more information.', true)
-        .addField('Purchase', 'You can purchase demi products, head over to #website for more. ', true)
-        .addField('Resellers', 'Looking to resell demi SB? Feel free to contact Lycolt / Staff Member for further assistance.', true)
-        .setTimestamp()
-        .setFooter(`Demi Support Bot`);
 
-    message.channel.send(embed);
-};
-
-module.exports.help = {
-    name: "help"
-};
-
-console.log('Supportconfig | Loaded')
+        message.channel.send(embed);
+    }
+}
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ client.on('ready', () =>{
     console.log(client.user.tag + ' Has Logged In Succesfully.')
 })
 
-const fs = require("fs");
+const fs = require('fs')
 client.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
@@ -15,15 +15,43 @@ for (const file of commandFiles) {
 
     client.commands.set(command.name, command);
 }
-client.on('message', message => {
-    if (!message.content.startsWith(process.env.PREFIX) || message.author.bot) return;
 
-    const args = message.content.slice(process.env.PREFIX.length).split(/ +/);
+client.on('message', message => {
+    if (!message.content.startsWith(process.env.BOT_PREFIX) || message.author.bot) return;
+
+    const args = message.content.slice(process.env.BOT_PREFIX.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
     if(client.commands.get(command)) {
-        client.commands.get(command).execute(client, message, args, Discord)
+        client.commands.get(command).execute(message, args, Discord)
     }
 });
+
+client.on('message', message =>{
+    if(message.content.startsWith('1')){
+        let em = new Discord.MessageEmbed()
+        .setAuthor('You cant invite the bot into the server.')
+        .addField('Find information and troubleshooting problems here:', '[Astroo Music Docs - Cant Invite](https://astroo.party/help.inviting.html)')
+        message.channel.send(embed=em)
+    }
+})
+
+client.on('message', message =>{
+    if(message.content.startsWith('2')){
+        let em = new Discord.MessageEmbed()
+        .setAuthor('The bot is not responding.')
+        .addField('Find information and troubleshooting problems here:', '[Astroo Music Docs - No Response](https://astroo.party/help.inviting.html)')
+        message.channel.send(embed=em)
+    }
+})
+
+client.on('message', message =>{
+    if(message.content.startsWith('3')){
+        let em = new Discord.MessageEmbed()
+        .setAuthor('You cant invite the bot into the server.')
+        .addField('Find information and troubleshooting problems here:', '[Astroo Music Docs - No Sound/Glitching](https://astroo.party/help.noresponse.html)')
+        message.channel.send(embed=em)
+    }
+})
 
 client.login(process.env.BOT_TOKEN)
